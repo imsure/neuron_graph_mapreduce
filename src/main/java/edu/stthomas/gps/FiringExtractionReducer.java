5daaf5be9ba52e1e421b2c443dd8c1155b444e18
Shuo Yang <imsure95@gmail.com>
2013-11-09 12:41:44 -0600

    Added job for firing extraction

---
+++ src/main/java/edu/stthomas/gps/FiringExtractionReducer.java
@@ -0,0 +1,19 @@
+package edu.stthomas.gps;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+public class FiringExtractionReducer
+extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+
+        @Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
+                        throws IOException, InterruptedException {
+                for (IntWritable value : values) {
+                        context.write(value, key);
+                }
+        }
+}
